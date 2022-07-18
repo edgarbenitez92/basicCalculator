21c9ad6dcb99e6599876562859004f7270536464
Edgar Benitez <edgarbenitez92@gmail.com>
2022-07-18 09:24:56 -0400

    Results operations and draft operation have been incorporated. Differents styles has been created

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-import { globalStyles } from '../theme/calculatorTheme'
+import { styles } from '../theme/calculatorTheme'
 
 export const CalculatorScreen = () => {
   return (
-    <View>
-      <Text style={globalStyles.text}>Calculator Screen</Text>
+    <View style={styles.calculatorContainer}>
+      <Text style={styles.currentOperation}>1,5000.00</Text>
+      <Text style={styles.prevOperation}>1,5000.00</Text>
+
+      <View>
+        <View style={styles.button}>
+          <Text style={styles.buttonLabel}>1</Text>
+        </View>
+      </View>
     </View>
   )
 }
--- src/theme/calculatorTheme.tsx
+++ src/theme/calculatorTheme.tsx
@@ -1,13 +1,37 @@
 import { StyleSheet } from "react-native";
 
-
-export const globalStyles = StyleSheet.create({
+export const styles = StyleSheet.create({
   backGround: {
     flex: 1,
     backgroundColor: 'black'
   },
-  text: {
+  calculatorContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+  },
+  currentOperation: {
     color: 'white',
-    fontSize: 20
+    fontSize: 60,
+    textAlign: 'right'
+  },
+  prevOperation: {
+    color: 'rgba(255, 255,255, 0.5)',
+    fontSize: 30,
+    textAlign: 'right'
+  },
+  button: {
+    backgroundColor: '#9B9B9B',
+    borderRadius: 100,
+    height: 80,
+    justifyContent: 'center',
+    width: 80
+  },
+  buttonLabel: {
+    color: 'black',
+    fontSize: 30,
+    fontWeight: '300',
+    padding: 10,
+    textAlign: 'center'
   }
 });
