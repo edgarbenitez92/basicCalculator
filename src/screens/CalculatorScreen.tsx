61ef271f1563b51a28c698aeec76f21137aca065
Edgar Benitez <edgarbenitez92@gmail.com>
2022-07-18 09:08:21 -0400

    calculatorScreen component creation. Setting this new component like a main component

---
+++ src/screens/CalculatorScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { globalStyles } from '../theme/calculatorTheme'
+
+export const CalculatorScreen = () => {
+  return (
+    <View>
+      <Text style={globalStyles.text}>Calculator Screen</Text>
+    </View>
+  )
+}
---
+++ src/theme/calculatorTheme.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+
+export const globalStyles = StyleSheet.create({
+  backGround: {
+    flex: 1,
+    backgroundColor: 'black'
+  },
+  text: {
+    color: 'white',
+    fontSize: 20
+  }
+});
