f7bb8426bc24fe01697160f256743a1f8864d38e
Edgar Benitez <edgarbenitez92@gmail.com>
2022-07-18 09:38:42 -0400

    All calculator basic buttons has been created. Additionally, a conditional width to the zero button and changes in colors buttons styles

--- src/components/CalcButton.tsx
+++ src/components/CalcButton.tsx
@@ -5,17 +5,24 @@ import { calcButton } from '../styles/calcButton';
 interface Props {
   task: string;
   color?: string;
+  isZeroButton?: boolean;
 }
 
-export const CalcButton = ({ task, color = '#2D2D2D' }: Props) => {
+export const CalcButton = ({ task, color = '#2D2D2D', isZeroButton = false }: Props) => {
   return (
     <View
       style={{
         ...calcButton.button,
-        backgroundColor: color
+        backgroundColor: color,
+        width: (isZeroButton) ? 100 : 80
       }}
     >
-      <Text style={calcButton.buttonLabel}>{task}</Text>
+      <Text style={{
+        ...calcButton.buttonLabel,
+        color: (color === '#9B9B9B') ? 'black' : 'white'
+      }}>
+        {task}
+      </Text>
     </View>
   )
 }
--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -9,11 +9,35 @@ export const CalculatorScreen = () => {
       <Text style={styles.prevOperation}>1,5000.00</Text>
       <Text style={styles.currentOperation}>1,5000.00</Text>
 
+      {/* Row Buttons */}
       <View style={styles.rowButtons}>
         <CalcButton task='C' color='#9B9B9B' />
         <CalcButton task='+/-' color='#9B9B9B' />
         <CalcButton task='del' color='#9B9B9B' />
-        <CalcButton task='/' color='#FF9427' />
+        <CalcButton task='/' color='rbg(255, 174, 0)' />
+      </View>
+
+      {/* Row Buttons */}
+      <View style={styles.rowButtons}>
+        <CalcButton task='7' />
+        <CalcButton task='8' />
+        <CalcButton task='9' />
+        <CalcButton task='X' color='rbg(255, 174, 0)' />
+      </View>
+
+      {/* Row Buttons */}
+      <View style={styles.rowButtons}>
+        <CalcButton task='4' />
+        <CalcButton task='5' />
+        <CalcButton task='6' />
+        <CalcButton task='-' color='rbg(255, 174, 0)' />
+      </View>
+
+      {/* Row Buttons */}
+      <View style={styles.rowButtons}>
+        <CalcButton task='0' />
+        <CalcButton task='.' />
+        <CalcButton task='=' color='rbg(255, 174, 0)' />
       </View>
     </View>
   )
