d9e3860be04ced86c8f6cb109a6bc53c70d5777e
Edgar Benitez <edgarbenitez92@gmail.com>
2022-07-18 09:51:37 -0400

    Various mathematical validations has been incorporated

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -5,32 +5,67 @@ import { styles } from '../theme/calculatorTheme'
 
 export const CalculatorScreen = () => {
 
-  const [currOperation, setCurrOperation] = useState('0');
-  const [prevOperation, setPrevOperation] = useState('100');
+  const [operation, setOperation] = useState('0');
+  const [recordOperation, setRecordOperation] = useState('100');
 
   const resetOperation = () => {
-    setCurrOperation('0');
+    setOperation('0');
   }
 
-  const setUpOperation = (numericButton: string) => {
-    setCurrOperation(currOperation + numericButton);
+  const setUpOperation = (pressedNumber: string) => {
+
+    // Validation: Only 1 dot
+    if (operation.includes('.') && pressedNumber === '.') return;
+
+    // Validation: Start with 0?
+    if (operation.startsWith('0') || operation.startsWith('-0')) {
+
+      // Validation: decimal + dot
+      if (pressedNumber === '.') {
+        setOperation(operation + pressedNumber);
+
+        // Validation: press 0 existing dot
+      } else if (pressedNumber === '0' && operation.includes('.')) {
+        setOperation(operation + pressedNumber)
+
+        // Validation: is different to zero? has a dot?
+      } else if (pressedNumber !== '0' && !operation.includes('.')) {
+        setOperation(pressedNumber);
+
+        // Validation: evade 0000000
+      } else if (pressedNumber === '0' && !operation.includes('.')) {
+        setOperation(operation);
+      } else {
+        setOperation(operation + pressedNumber);
+      }
+    } else {
+      setOperation(operation + pressedNumber);
+    }
+  }
+
+  const changeTypeNumber = () => {
+    if (operation.includes('-')) {
+      setOperation(operation.replace('-', ''));
+    } else {
+      setOperation('-' + operation);
+    }
   }
 
   return (
     <View style={styles.calculatorContainer}>
-      <Text style={styles.prevOperation}>{prevOperation}</Text>
+      <Text style={styles.prevOperation}>{recordOperation}</Text>
       <Text
         style={styles.currentOperation}
         numberOfLines={1}
         adjustsFontSizeToFit
       >
-        {currOperation}
+        {operation}
       </Text>
 
       {/* Row Buttons */}
       <View style={styles.rowButtons}>
         <CalcButton task='C' color='#9B9B9B' action={resetOperation} />
-        <CalcButton task='+/-' color='#9B9B9B' action={resetOperation} />
+        <CalcButton task='+/-' color='#9B9B9B' action={changeTypeNumber} />
         <CalcButton task='del' color='#9B9B9B' action={resetOperation} />
         <CalcButton task='/' color='rbg(255, 174, 0)' action={resetOperation} />
       </View>
